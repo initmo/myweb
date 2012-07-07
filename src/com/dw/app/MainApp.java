8659273a7906e06596efbbcfdead0f53814d698a
initmo <initmo@gmail.com>
2012-07-07 16:17:03 +0800

    Revert "add class UseageApp.java"
    This reverts commit 3ca76736c1aee2345d0c8ea3ce3c67649df18c5d.

--- src/com/dw/app/MainApp.java
+++ src/com/dw/app/MainApp.java
@@ -6,7 +6,8 @@ public class MainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("this is a main application.");
+		// TODO Auto-generated method stub
+
 	}
 
 }
